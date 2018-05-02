8f14e6d13d1f14c99c84ed60c551c8174d44aa21
zhonghua_he <he.zhonghua.0921@gmail.com>
2018-05-02 17:16:47 +0800

    chapter 3

---
+++ exer3_3.py
@@ -0,0 +1,11 @@
+def print_line(a, b):
+	print(a, b, b, b, b, a, b, b, b, b, a)
+
+
+print_line('+', '-')
+for i in range(4):
+	print_line('|', ' ')
+print_line('+', '-')
+for i in range(4):
+	print_line('|', ' ')
+print_line('+', '-')
